03aec653d0f58908935a88d783a4b253948ffd4f
Kevin Marchais <kevinmarchais@gmail.com>
2025-01-08 11:46:48 +0100

    fix verison and ci

--- src/template_extension_module/__init__.py
+++ src/template_extension_module/__init__.py
@@ -2,10 +2,8 @@
 
 import logging
 
-from template_extension_module import _version
 from template_extension_module._core import hello_from_bin
-
-__version__ = _version.__version__
+from template_extension_module._version import __version__
 
 logging.basicConfig(
     level=logging.INFO,
